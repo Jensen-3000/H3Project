96efe700339955cee8f65e902b58cf47bca8551c
Jensen-3000 <118268364+Jensen-3000@users.noreply.github.com>
2024-11-22 12:15:45 +0100

    add: first draft of the controller.

---
+++ H3Project.Data/Repository/TicketRepository.cs
@@ -0,0 +1,47 @@
+﻿using H3Project.Data.Context;
+using H3Project.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace H3Project.Data.Repository;
+
+public class TicketRepository
+{
+    private readonly CinemaDbContext _dbContext;
+
+    public TicketRepository(CinemaDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Ticket>> GetAllTicketsAsync()
+    {
+        return await _dbContext.Tickets.ToListAsync();
+    }
+
+    public async Task<Ticket?> GetTicketByIdAsync(int id)
+    {
+        return await _dbContext.Tickets.FirstOrDefaultAsync(t => t.TicketId == id);
+    }
+
+    public async Task AddTicketAsync(Ticket ticket)
+    {
+        await _dbContext.Tickets.AddAsync(ticket);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateTicketAsync(Ticket ticket)
+    {
+        _dbContext.Tickets.Update(ticket);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteTicketAsync(int id)
+    {
+        var ticket = await _dbContext.Tickets.FindAsync(id);
+        if (ticket != null)
+        {
+            _dbContext.Tickets.Remove(ticket);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
